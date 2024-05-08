2529fe0eb76f91a0766edc40a34c09a9c5b267bd
Alex Zuo <alexanderhzuo@gmail.com>
2024-05-08 13:50:45 -0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Alex_URLShortener
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var urls = make(map[string]string)
+var serverLoc = "3030"
+var homeURL = fmt.Sprintf("http://localhost:%s", serverLoc)
+
+/*
+* Need a main page for user to input url
+Need a page after that url gets inputted that shows original url and now new url.
+Lastly, need a page to process that shortened url and "spit out" original url
+*/
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/shorturl", handleNewURL)
+	http.HandleFunc("/short/", handleRedirect)
+	fmt.Println("URL Shortener is running on :" + serverLoc)
+	http.ListenAndServe(":"+serverLoc, nil)
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		http.Redirect(w, r, "/shorturl", http.StatusSeeOther)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+
+	//title is what the tab is called.
+	//body is what shows up on the page
+	//form creates a form and the method determines what kind of http call it is.
+	fmt.Fprint(w, `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Simple URL Shortener</title>
+		</head>
+		<body>
+			<h2>URL Shortener</h2>
+			<form id="urlForm" method="post" action="/shorturl">
+				<div class="field" id="url"
+					<label for="url">Original URL:</label>
+					<input type="url" name="url" placeholder="Enter a URL" required">
+				</div>
+				<div class="field" id="urlOption"
+					<label for="urlOption">How would you like the url to be generated?</label>
+					<select id="urlOption" name="urlOption" onchange = "shortURLVisibility()">
+					<option>Auto-Generate</option>
+					<option>User Input</option>
+					</select>
+					<script>
+					function shortURLVisibility() {
+						var shortenedURL = document.getElementById("shortenedURL");
+						var visibility = shortenedURL.style.display;
+						if (visibility == "none")  {
+							shortenedURL.style.display = "block";
+							URLAvailability.style.display = "block";
+						}
+						else {
+							shortenedURL.style.display = "none";
+							URLAvailability.style.display = "none";
+						}
+					}
+					</script>
+				</div>
+				<div class="field" id="shortenedURL" style="display: none;"
+					<label for="shortenedURL"> Shortened URL: http://localhost:3030/short/ </label>
+					<input type="text" name="shortenedURL" placeholder="Enter a key for the new url">
+				</div>
+				<p>
+					<input type="submit" value="Shorten">
+				</p>
+			</form>
+		</body>
+		</html>
+	`)
+}
+
+func handleNewURL(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Check if the url structure is valid
+	originalURL := r.FormValue("url")
+	if !isUrl(originalURL) {
+		http.Error(w, "Not a valid url", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the url is a valid/public site
+	if !isReachable(originalURL) {
+		http.Error(w, "Not a valid url", http.StatusBadRequest)
+		return
+	}
+
+	// Generate a unique shortened key for the original URL
+	var shortKey string
+	if r.FormValue(("urlOption")) == "Auto-Generate" {
+		shortKey = generateShortKey()
+	} else {
+		shortKey = r.FormValue("shortenedURL")
+	}
+	if urls[shortKey] != "" {
+		fmt.Fprint(w, `
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>URL Shortener</title>
+			</head>
+			<body>
+				<h2> <a href="`, homeURL, `">`, "URL Shortener", `</a> </h2>
+			</body>
+			</html>
+		`)
+		http.Error(w, "URL is already in use", http.StatusBadRequest)
+		return
+	}
+
+	// Make sure that key is not already in the map. If it's in the map, keep generating new keys.
+	// val := urls[shortKey]
+	// for val != "" {
+	// 	shortKey := generateShortKey()
+	// 	val = urls[shortKey]
+	// }
+	urls[shortKey] = originalURL
+
+	// Construct the full shortened URL
+	shortenedURL := fmt.Sprintf("http://localhost:%s/short/%s", serverLoc, shortKey)
+
+	fmt.Fprint(w, `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>URL Shortener</title>
+		</head>
+		<body>
+			<h2> <a href="`, homeURL, `">`, "URL Shortener", `</a> </h2>
+			<p> Original URL: `, originalURL, `</p> 
+			<p> Shortened URL: <a href="`, shortenedURL, `">`, shortenedURL, `</a></p>
+		</body>
+		</html>
+	`)
+}
+
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	shortKey := strings.TrimPrefix(r.URL.Path, "/short/")
+	if shortKey == "" {
+		http.Error(w, "Shortened key is missing", http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the original URL from the `urls` map using the shortened key
+	originalURL, found := urls[shortKey]
+	if !found {
+		http.Error(w, "Shortened key not found", http.StatusNotFound)
+		return
+	}
+
+	// Redirect the user to the original URL
+	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
+}
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 4
+	const backTruncAmount = 1000
+	const frontTruncAmount = 1000000000
+	curTime := time.Now().UnixMilli()
+	frontTruncTime := curTime / frontTruncAmount
+	backTruncTime := curTime % backTruncAmount
+	newRand := rand.New(rand.NewSource(curTime))
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[newRand.Intn(len(charset))]
+	}
+	return strconv.FormatInt(frontTruncTime, 10) + strconv.FormatInt(backTruncTime, 10) + string(shortKey)
+}
+
+func isUrl(testUrl string) bool {
+	u, err := url.ParseRequestURI(testUrl)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+func isReachable(testUrl string) bool {
+	resp, err := http.Get(testUrl)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	} else {
+		fmt.Printf("\nStatus code is: %d \nStatus is: %s", resp.StatusCode, resp.Status)
+		return true
+	}
+}
