33190b4399961ee6d9c807d7a25a04e1f9cd29a2
ahzuo <alexanderhzuo@gmail.com>
2024-06-21 10:31:33 -0700

    Get rid of unneeded comments

--- main.go
+++ main.go
@@ -247,29 +247,3 @@ func isReachable(testUrl string) bool {
 		return true
 	}
 }
-
-// Need to add a db to this so that data is hosted there instead of in this instance whenever the exe runs. Look into postgres or MySQL.
-// var db *sql.DB
-
-// func main() {
-//     // Capture connection properties.
-//     cfg := mysql.Config{
-//         User:   os.Getenv("DBUSER"),
-//         Passwd: os.Getenv("DBPASS"),
-//         Net:    "tcp",
-//         Addr:   "127.0.0.1:3306",
-//         DBName: "recordings",
-//     }
-//     // Get a database handle.
-//     var err error
-//     db, err = sql.Open("mysql", cfg.FormatDSN())
-//     if err != nil {
-//         log.Fatal(err)
-//     }
-
-//     pingErr := db.Ping()
-//     if pingErr != nil {
-//         log.Fatal(pingErr)
-//     }
-//     fmt.Println("Connected!")
-// }
